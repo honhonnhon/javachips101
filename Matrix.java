da6d8c313cd79743ce95af24a5434ffa801e0d30
honhonnhon <kornsup@windowslive.com>
2022-11-26 14:58:03 +0700

    add MaxMix program

--- Matrix.java
+++ Matrix.java
@@ -1,9 +1,7 @@
 public class Matrix {
     public static void main(String[] args) {
-        int x;
-        int y;
-        for(y=1;y<=5;y++){
-            for(x=1;x<=5;x++){
+        for(int y=1;y<=5;y++){
+            for(int x=1;x<=5;x++){
                 if (x!=y){
                     System.out.print(" 0 "); 
                 }
---
+++ MaxMin.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MaxMin {
+    public static void main(String[] args) {
+        Scanner key = new Scanner(System.in);
+        int Maxnum = Integer.MIN_VALUE, Minnum = Integer.MAX_VALUE;
+        while(true){
+            System.out.print("Enter number : ");
+            int num = key.nextInt();
+            if (num<0)break;
+            if (num>Maxnum)Maxnum = num;
+            if (num<Maxnum)Minnum = num;
+        }
+        System.out.println("Max number = "+Maxnum);
+        System.out.println("Min number = "+Minnum);
+        key.close();
+    }
+}
